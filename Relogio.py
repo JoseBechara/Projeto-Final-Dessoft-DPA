9f7908e08e9d63e472c9ed50d6bed96a3a5dd788
omardcan <38349951+omardcan@users.noreply.github.com>
2018-11-13 17:09:10 -0200

    enviando de novo

--- Relogio.py
+++ Relogio.py
@@ -20,7 +20,7 @@ class Clock:
         self.clock = tk.Label(self.janela, font=("times", 50, "bold"),fg = "white", bg = "red")
     
     def tick(self):
-        time_string = time.strftime("%A  %B %d \n %Y %H:%M")
+        time_string = time.strftime("(diadasemana[%A])  (meses[%B]) %d \n %Y %H:%M")
         self.clock.config(text = time_string)
         self.clock.after(200, self.tick)
     
---
+++ Traducao_relogio.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 17:01:29 2018
+
+@author: OmarDiboCalixtoAfrangeNeto
+"""
+
